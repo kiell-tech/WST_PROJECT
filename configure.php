42a095d5f608668286823ad4ac62a8e22556b3ad
vennbtwn <ravenlou08@gmail.com>
2026-03-20 22:06:59 +0800

    BINAGO NA SI CONFIGURE NI ESMA

--- configure.php
+++ configure.php
@@ -4,7 +4,20 @@ define('DB_PASS', '');        // leave blank if no MySQL password set
 define('DB_NAME', 'bulsu_announcements');
 define('APP_URL',  'http://localhost/bulacan_announcements');
 ```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1> WST PROJJJJJJ  GOES BRUTTTT </h1>
+</body>
+</html>
+
 
 **4. Visit the site**
 ```
-http://localhost/bulacan_announcements/
+http://localhost/bulacan_announcements/  
