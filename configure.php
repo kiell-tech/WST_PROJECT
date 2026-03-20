7058047b0483faf3d99553c99d7df39b56042780
kiell-tech <ekielconcon@gmail.com>
2026-03-20 22:07:15 +0800

    naglagay ako ng hello world kay raven

--- configure.php
+++ configure.php
@@ -1,10 +1,13 @@
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');        // leave blank if no MySQL password set
-define('DB_NAME', 'bulsu_announcements');
-define('APP_URL',  'http://localhost/bulacan_announcements');
-```
-
-**4. Visit the site**
-```
-http://localhost/bulacan_announcements/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>EYYY ANG TUFF TUFF TALAGA NI ESMA</h1>
+    <h2>Hello World</h2>
+</body>
+</html>
